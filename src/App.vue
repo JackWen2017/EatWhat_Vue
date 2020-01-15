8873d8ef2a0316d5f94d43954a8009b07dac82c0
jack <jack@hotmail.com>
2020-01-15 22:27:09 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Restaurant />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Restaurant from "@/pages/Restaurant.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Restaurant
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: rgb(58, 232, 255);
+}
+input {
+  border: none;
+  text-decoration: none;
+  border-radius: 6px;
+  height: 22px;
+  font-size: 14px;
 }
 </style>
---
+++ src/pages/Restaurant.vue
@@ -0,0 +1,24 @@
+ <template>
+  <div>
+    <div class="restaurant">資料讀取中...</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.restaurant {
+  background: rgb(28, 206, 152);
+  border: 3px solid rgb(19, 10, 145);
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px cornflowerblue;
+  width: 95%;
+  margin: auto;
+  text-align: center;
+  line-height: 90vh;
+  font-size: 23px;
+  color: blue;
+}
+</style>
